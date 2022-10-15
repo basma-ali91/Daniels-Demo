2c6d829e5921a8182d7c872dff3b38e8d1d6668b
basma-ali91 <basmaali01@gmail.com>
2022-10-15 19:52:57 +0200

    new js code

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ window.addEventListener("scroll", function () {
     let home = document.getElementById("home");
     let scrollNav = window.scrollY;
     let homeHeight = home.scrollHeight;
+    
     let toggeler = document.getElementById("toggler");
 
 
@@ -14,5 +15,83 @@ window.addEventListener("scroll", function () {
     else {
         navbar.style.backgroundColor = "transparent";
     }
-    
+
 })
+
+
+// ----------------------counter section-----------
+
+// window.addEventListener("scroll",function(){
+//     let numberSction=document.getElementById("number");
+//     let numberSectionHeight=numberSction.scrollTop;
+
+//     console.log(numberSectionHeight)
+
+    // if(window.body.scrollTop>numberSectionHeight){
+    //     console.log("hi")
+    // }
+    
+
+
+
+    
+// })
+
+
+let counter = 0;
+let counter1 = 0;
+let counter2 = 0;
+let counter3 = 0;
+function countCustomer() {
+
+    let happy_customer = document.getElementById("happy");
+    counter++;
+    happy_customer.innerHTML = counter;
+    if (counter == 850) {
+        clearInterval(customerHappy)
+    }
+}
+
+let customerHappy = setInterval(() => {
+    countCustomer()
+}, 3);
+
+function completePro() {
+
+    let complete = document.getElementById("complete");
+    counter1++;
+    complete.innerHTML = counter1;
+    if (counter1 == 230) {
+        clearInterval(completproject)
+    }
+}
+
+let completproject = setInterval(() => {
+    completePro()
+}, 15);
+
+function linesOfCodes() {
+    let lines = document.getElementById("lines");
+    counter2++;
+    lines.innerHTML = counter2;
+    if (counter2 == 945) {
+        clearInterval(linesCode)
+    }
+}
+
+let linesCode = setInterval(() => {
+    linesOfCodes()
+}, 3);
+
+function downloadFiles() {
+    let download = document.getElementById("download")
+    counter3++;
+    download.innerHTML = counter3
+    if (counter3 == 780) {
+        clearInterval(down)
+    }
+}
+
+let down = setInterval(() => {
+    downloadFiles()
+}, 3);
